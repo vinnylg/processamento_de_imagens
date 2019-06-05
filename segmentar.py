41a98e53780c690f05fda726fc41925498c2735e
Vinicius de Lima Gonçalves <vlg17@inf.ufpr.br>
2019-06-05 17:00:26 -0300

    shit that i did

--- segmentar.py
+++ segmentar.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 import os
-import cv2 as cv
+import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -12,36 +13,22 @@ def read_files(path):
 	return files			
 
 
-
-
-
 def main():
-
-	print("aaa")
 
 	files = read_files("database")
 
 	for f in files:
-		imgIn = cv.imread(f)
-		hsv = cv2.cvtColor(img_in, cv2.COLOR_BGR2HSV)
+		imgIn = cv2.imread(f)
+		hsv = cv2.cvtColor(imgIn, cv2.COLOR_BGR2HSV)
 
 		lower_blue = 100, 150, 0
 		upper_blue = 140, 255, 255
 
 		th = cv2.inRange(hsv, lower_blue, upper_blue)
 
-		cv2.imshow('th', th)
-		cv2.waitKey()
+		cv2.imshow('a',th)
 
 		mask = 255 - th
-
-		cv2.imshow('mask',mask)
-		cv2.waitKey()
-
-		img_m = cv2.bitwise_and(img_in,img_in, mask= mask)
-
-		cv2.imshow('img_m',img_m)
-		cv2.waitKey()
 
 		ctrs, hier = cv2.findContours(th.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -49,15 +36,26 @@ def main():
 		sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
 
 		for i, ctr in enumerate(sorted_ctrs):
-		    # Get bounding box
-		    x, y, w, h = cv2.boundingRect(ctr)
+			x, y, w, h = cv2.boundingRect(ctr)
+			roi = th[y:y + h, x:x + w]
 
-		    # Getting ROI
-		    roi = img_m[y:y + h, x:x + w]
+			if( w > 20 ):
+				ctrs2, hier2 = cv2.findContours(roi.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+				sorted_ctrs2 = sorted(ctrs2, key=lambda ctr2: cv2.boundingRect(ctr2)[0])		
 
-		    # show ROI
-		    # cv2.imshow('segment no:'+str(i),roi)
-		    # cv2.rectangle(img_m, (x, y), (x + w, y + h), (0, 255, 0), 2)
+				for i2, ctr2 in enumerate(sorted_ctrs2):
+					x2, y2, w2, h2 = cv2.boundingRect(ctr2) 
+					roi2 = th[y2:y2+h2, x2:x2+w2] 
+
+					if w2 < 20 and h2 < 20:
+						roi[y2:y2+h2, x2:x2+w2] = 0
+			
+					mask = 255 - roi
+					roi_m = cv2.bitwise_and(roi,roi, mask= mask)
+					print(f.strip('.jpg')+str(i)+'.jpg')
+					nome = f.split('/')
+					nome = nome[2]
+					cv2.imwrite(nome+'-'+str(i)+'.jpg',roi_m)
 
 if __name__== "__main__":
   main()
